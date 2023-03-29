0dbd49cff4faf0c61451f0aa247f36913f121498
Guillermo Castro <guillermo.castro.19@um.edu.mt>
2023-03-29 20:49:02 +0200

    Add fix for cache memory bug

--- CurrencyExchangeMicroservice/CurrencyExchangeMicroservice/Program.cs
+++ CurrencyExchangeMicroservice/CurrencyExchangeMicroservice/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
+builder.Services.AddMemoryCache();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
