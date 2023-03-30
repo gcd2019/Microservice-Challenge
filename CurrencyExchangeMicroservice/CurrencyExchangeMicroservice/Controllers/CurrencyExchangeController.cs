d0e00407993d994621b38369d2a9d03f692811ee
Guillermo Castro <guillermo.castro.19@um.edu.mt>
2023-03-30 11:14:47 +0200

    Add Delete Request

--- CurrencyExchangeMicroservice/CurrencyExchangeMicroservice/Controllers/CurrencyExchangeController.cs
+++ CurrencyExchangeMicroservice/CurrencyExchangeMicroservice/Controllers/CurrencyExchangeController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 
 
 namespace CurrencyExchangeMicroservice.Controllers
@@ -19,12 +20,14 @@ namespace CurrencyExchangeMicroservice.Controllers
         private readonly FixerApiClient _fixerApiClient;
         private readonly CurrencyExchangeDbContext _context;
         private readonly IMemoryCache _memoryCache;
+        private readonly ILogger<CurrencyExchangeController> _logger;
 
-        public CurrencyExchangeController(FixerApiClient fixerApiClient, CurrencyExchangeDbContext context, IMemoryCache memoryCache)
+        public CurrencyExchangeController(FixerApiClient fixerApiClient, CurrencyExchangeDbContext context, IMemoryCache memoryCache, ILogger<CurrencyExchangeController> logger)
         {
             _fixerApiClient = fixerApiClient;
             _context = context;
             _memoryCache = memoryCache;
+            _logger = logger;
         }
 
         [HttpPost]
@@ -32,11 +35,14 @@ namespace CurrencyExchangeMicroservice.Controllers
         {
             try
             {
+                _logger.LogInformation("Received currency exchange request: {@Request}", request);
+                
                 var clientId = GetClientId();
 
                 // Check if the client has exceeded the rate limit
                 if (!IsClientAllowedToTrade(clientId))
                 {
+                    _logger.LogWarning("Client {ClientId} exceeded rate limit", clientId);
                     return StatusCode(429, "You have exceeded the limit of 10 trades per hour.");
                 }
 
@@ -64,10 +70,13 @@ namespace CurrencyExchangeMicroservice.Controllers
                 await _context.SaveChangesAsync();
                 IncrementTradeCount(clientId);
 
+                _logger.LogInformation("Currency exchange completed successfully: {@Response}", response);
+
                 return Ok(response);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error occurred during currency exchange: {Message}", ex.Message);
                 return BadRequest($"Error: {ex.Message}");
             }
         }
@@ -76,6 +85,7 @@ namespace CurrencyExchangeMicroservice.Controllers
         [HttpGet("trades")]
         public async Task<ActionResult<IEnumerable<CurrencyExchangeTrade>>> GetTrades()
         {
+            _logger.LogInformation("Retrieving trade history");
             return Ok(await _context.CurrencyExchangeTrades.ToListAsync());
         }
 
@@ -106,13 +116,35 @@ namespace CurrencyExchangeMicroservice.Controllers
             {
                 // Cache entry not found, add a new entry with a count of 1
                 _memoryCache.Set(clientId, 1, TimeSpan.FromMinutes(cacheExpirationMinutes));
+                _logger.LogInformation("Client {ClientId} cache entry created with trade count 1", clientId);
             }
             else
             {
                 // Increment the trade count and update the cache entry
                 _memoryCache.Set(clientId, tradeCount + 1, TimeSpan.FromMinutes(cacheExpirationMinutes));
+                _logger.LogInformation("Client {ClientId} trade count incremented to {TradeCount}", clientId, tradeCount + 1);
             }
         }
 
+        // Add a new endpoint to delete all trade history
+        [HttpDelete("trades")]
+        public async Task<IActionResult> DeleteAllTrades()
+        {
+            _logger.LogInformation("Deleting all trade history");
+
+            var trades = _context.CurrencyExchangeTrades;
+
+            if (!trades.Any())
+            {
+                return NotFound("No trades found to delete.");
+            }
+
+            _context.CurrencyExchangeTrades.RemoveRange(trades);
+            await _context.SaveChangesAsync();
+
+            _logger.LogInformation("All trade history deleted successfully"); 
+
+            return Ok("All trade history deleted successfully");
+        }
     }
 }
--- CurrencyExchangeMicroservice/CurrencyExchangeMicroservice/FixerApiClient.cs
+++ CurrencyExchangeMicroservice/CurrencyExchangeMicroservice/FixerApiClient.cs
@@ -12,26 +12,32 @@ namespace CurrencyExchangeMicroservice
         private const string BaseUrl = "https://api.apilayer.com/fixer/";
 
         private readonly HttpClient _httpClient;
+        private readonly ILogger<FixerApiClient> _logger;
 
-        public FixerApiClient(HttpClient httpClient, IConfiguration configuration)
+        public FixerApiClient(HttpClient httpClient, IConfiguration configuration, ILogger<FixerApiClient> logger)
         {
             _httpClient = httpClient;
             ApiKey = configuration.GetValue<string>("FixerApi:ApiKey");
+            _logger = logger;
         }
 
         private readonly ConcurrentDictionary<string, (decimal Rate, DateTime Timestamp)> _exchangeRateCache = new();
 
         public async Task<decimal> GetExchangeRate(string fromCurrency, string toCurrency, decimal amount)
         {
+            _logger.LogInformation("Getting exchange rate for {FromCurrency} to {ToCurrency}", fromCurrency, toCurrency);
+
             string cacheKey = $"{fromCurrency}-{toCurrency}";
             if (_exchangeRateCache.TryGetValue(cacheKey, out var rateInfo) && !IsRateExpired(rateInfo.Timestamp))
             {
+                _logger.LogInformation("Using cached exchange rate: {Rate}", rateInfo.Rate);
                 return rateInfo.Rate;
             }
             else
             {
                 decimal newRate = await GetExchangeRateFromApi(fromCurrency, toCurrency, amount);
                 _exchangeRateCache[cacheKey] = (newRate, DateTime.UtcNow);
+                _logger.LogInformation("Fetched new exchange rate from API: {Rate}", newRate);
                 return newRate;
             }
         }
@@ -53,6 +59,7 @@ namespace CurrencyExchangeMicroservice
 
             if (!response.IsSuccessful)
             {
+                _logger.LogError("Error getting exchange rate from API: {ErrorMessage}", response.ErrorMessage);
                 throw new Exception($"Error getting exchange rate: {response.ErrorMessage}");
             }
 
